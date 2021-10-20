b43b3854b1f3bd2dbc0a179d9f2a6a87761c5dec
Qiming Liu <qiming.liu@student.adelaide.edu.au>
2021-10-20 14:56:44 +1030

    Update https ssl

--- backend/app.py
+++ backend/app.py
@@ -153,4 +153,4 @@ def search():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, use_reloader=False)
+    app.run(debug=True, use_reloader=False, host='0.0.0.0', port=443, ssl_context=('secret.pem', 'secret.key'))
