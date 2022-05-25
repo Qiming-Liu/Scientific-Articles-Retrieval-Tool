7825d49ca1a0f6ed402f936d54149755506c3a5b
Qiming-Liu <prossliu@gmail.com>
2022-05-26 04:04:05 +0930

    feat: finished

--- backend/neo4j/init.cmd
+++ backend/neo4j/init.cmd
@@ -1,3 +1,3 @@
-docker exec -it neo4j bin/neo4j-admin import --database=minio --nodes=import/minie-entity.csv --relationships=import/minie-relation.csv
+docker exec -it neo4j bin/neo4j-admin import --database=minio --nodes=import/entity.csv --relationships=import/relation.csv
 docker exec -it neo4j bin/cypher-shell -u neo4j -p 123456 "CREATE DATABASE minio"
 pause
